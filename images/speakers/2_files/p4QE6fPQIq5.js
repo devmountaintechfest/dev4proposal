b54008cb261a266028dc78faff07f8a07992b47d
Apaichon Punopas <a677018@AR1000677018N1.local>
2024-11-16 16:00:31 +0700

    init v1.0

---
+++ images/speakers/2_files/p4QE6fPQIq5.js
@@ -0,0 +1,6 @@
+;/*FB_PKG_DELIM*/
+
+__d("XCometLWIProductPickerControllerRouteBuilder",["jsRouteBuilder"],(function(a,b,c,d,e,f,g){a=c("jsRouteBuilder")("/ad_center/create/ad/",Object.freeze({use_template_post:!1}),void 0);b=a;g["default"]=b}),98);
+__d("XCometPageVanityControllerRouteBuilder",["jsRouteBuilder"],(function(a,b,c,d,e,f,g){a=c("jsRouteBuilder")("/{vanity}/",Object.freeze({ref:""}),void 0);b=a;g["default"]=b}),98);
+__d("XCometProfessionalDashboardInsightsHomeControllerRouteBuilder",["jsRouteBuilder"],(function(a,b,c,d,e,f,g){a=c("jsRouteBuilder")("/professional_dashboard/insights/",Object.freeze({}),void 0);b=a;g["default"]=b}),98);
+__d("XCometProfessionalDashboardOverviewControllerRouteBuilder",["jsRouteBuilder"],(function(a,b,c,d,e,f,g){a=c("jsRouteBuilder")("/professional_dashboard/overview/",Object.freeze({}),void 0);b=a;g["default"]=b}),98);
